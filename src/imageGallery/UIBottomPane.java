afae2344ce5d4851142cbffa05de2831222d9b28
Eric Nunez <ericnunez@gmail.com>
2019-04-08 18:38:45 -0500

    changed class name

---
+++ src/imageGallery/UIBottomPane.java
@@ -0,0 +1,18 @@
+package imageGallery;
+
+import javafx.scene.layout.HBox;
+
+import java.net.URI;
+
+public class UIBottomPane {
+    URI filePath;
+    HBox hbox = new HBox();
+
+    public UIBottomPane(URI filePath) {
+        this.filePath = filePath;
+    }
+
+    public void setupPane() {
+
+    }
+}
