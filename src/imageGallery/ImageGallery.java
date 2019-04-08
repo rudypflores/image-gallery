f131dde922e88dc620c62652c6734c4a3cb0ec9d
rudypflores <rudypichola@gmail.com>
2019-04-08 18:34:22 -0500

    Creation of menu bar

--- src/imageGallery/ImageGallery.java
+++ src/imageGallery/ImageGallery.java
@@ -2,6 +2,7 @@ package imageGallery;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.MenuBar;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -16,6 +17,12 @@ public class ImageGallery extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+
+        //Draw the top menu bar
+        UIMenu uiMenuBar = new UIMenu();
+        MenuBar topMenuBar = uiMenuBar.drawMenuBar();
+
+        pane.setTop(topMenuBar);
 
         primaryStage.setScene(scene);
         primaryStage.setTitle("Image Gallery");
