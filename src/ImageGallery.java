03c41bc47d3c4a3d6a6c28b3bdaaa3d8e74826e6
rudypflores <rudypichola@gmail.com>
2019-04-08 18:04:14 -0500

    Addition of BorderPane

---
+++ src/ImageGallery.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class ImageGallery extends Application {
+
+    BorderPane pane = new BorderPane();
+    Scene scene = new Scene(pane, 1280, 720);
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Image Gallery");
+        primaryStage.show();
+    }
+}
