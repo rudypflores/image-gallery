6f8c17039ad3ea2ef749c5fce44f44280e4e8c08
tru nguyen <nguyenquoctru99@gmail.com>
2019-04-16 15:22:26 -0500

    finshed the arrow event

--- src/imageGallery/ImageGallery.java
+++ src/imageGallery/ImageGallery.java
@@ -1,19 +1,18 @@
 package imageGallery;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.MenuBar;
 
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
 
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.util.List;
@@ -23,6 +22,8 @@ public class ImageGallery extends Application {
 
     BorderPane pane = new BorderPane();
     Scene scene = new Scene(pane, 1280, 720);
+    private int currentIndex = 0;
+
 
     public static void main(String[] args) {
         launch(args);
@@ -35,12 +36,6 @@ public class ImageGallery extends Application {
         UIMenu uiMenuBar = new UIMenu();
         MenuBar topMenuBar = uiMenuBar.drawMenuBar();
 
-
-        //set up middle
-        pane.setRight(UIimage.loadImage("http://www.stickpng.com/assets/images/585e46c3cb11b227491c3377.png",0));
-        pane.setLeft(UIimage.loadImage("http://www.stickpng.com/assets/images/585e46c3cb11b227491c3377.png",180));
-        
-
         pane.setTop(topMenuBar);
 
         // Set up Everything needed for the bottom pane
@@ -51,6 +46,7 @@ public class ImageGallery extends Application {
         List<Node> list = hbox.getChildren();
 
         // add event listeners for each image
+        int i=0;
         for (Node n: list) {
             ImageView imageView = (ImageView) n;
             Image innerImage = imageView.getImage();
@@ -59,11 +55,34 @@ public class ImageGallery extends Application {
             centerImageView.setFitHeight(500);
             centerImageView.setFitWidth(500);
 
+            final int a = i;
             imageView.setOnMouseClicked(event -> {
-
                 pane.setCenter(new UIimage(0).loadImage(centerImageView));
+                this.currentIndex = a;
             });
+            i++;
         }
+
+        //set up middle
+        pane.setRight(UIimage.loadArrow("http://www.stickpng.com/assets/images/585e46c3cb11b227491c3377.png",0));
+        pane.setLeft(UIimage.loadArrow("http://www.stickpng.com/assets/images/585e46c3cb11b227491c3377.png",180));
+
+        //set up eventHandler for right and left
+        StackPane right = (StackPane) pane.getRight();
+        StackPane left = (StackPane) pane.getLeft();
+        right.setOnMouseClicked(v -> {
+            int index = (this.currentIndex + 1) % list.size();
+             pane.setCenter(new UIimage(0).switchImage((ImageView) list.get(index)));
+             this.currentIndex = index;
+        });
+        left.setOnMouseClicked(v -> {
+            int index = (this.currentIndex - 1) % list.size();
+            index = (index < 0) ? list.size()-1 : index;
+            pane.setCenter(new UIimage(0).switchImage((ImageView) list.get(index)));
+            this.currentIndex = index;
+        });
+
+
 
         pane.setTop(topMenuBar);
         pane.setBottom(scrollPane);
@@ -72,4 +91,6 @@ public class ImageGallery extends Application {
         primaryStage.setTitle("Image Gallery");
         primaryStage.show();
     }
+
+
 }
--- src/imageGallery/UIimage.java
+++ src/imageGallery/UIimage.java
@@ -1,13 +1,15 @@
 package imageGallery;
 
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 
 public class UIimage
 {
-
+    private int i;
     private double degree;
 
     public UIimage(double degree)
@@ -25,14 +27,28 @@ public class UIimage
         return root;
     }
 
-    public static StackPane loadImage(String url, double degree)
+    public StackPane switchImage(ImageView img)
+    {
+        ImageView imageView = img;
+        Image innerImage = imageView.getImage();
+        ImageView centerImageView = new ImageView(innerImage);
+        centerImageView.setPreserveRatio(true);
+        centerImageView.setFitHeight(500);
+        centerImageView.setFitWidth(500);
+
+        return this.loadImage(centerImageView);
+    }
+
+
+    public static StackPane loadArrow(String url, double degree)
     {
         ImageView img = new ImageView(new Image(url));
-        img.setFitWidth(20);
-        img.setFitHeight(20);
+        img.setFitWidth(60);
+        img.setFitHeight(60);
         img.setRotate(degree);
 
         StackPane root = new StackPane();
+        root.setPadding(new Insets(0,80,0,80));
         root.getChildren().add(img);
 
         return root;
@@ -41,5 +57,10 @@ public class UIimage
     public double getDgree()
     {
         return this.degree;
+    }
+
+    public int getI()
+    {
+        return this.i;
     }
 }
